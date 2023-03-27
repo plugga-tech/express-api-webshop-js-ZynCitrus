b8d29136a2ca4f5f1b2a2ea4805dfa1115c1eb68
Sebastian Post <sebastianpost@protonmail.com>
2023-03-27 12:13:28 +0200

    Småfix, allt funkar utom hämta en specifik produkt

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { ObjectId } = require("mongodb");
+var { ObjectId } = require('mongodb');
 
 /* Hämtar ALLA användare */
 router.get('/users', function(req, res, next) {
@@ -10,21 +10,22 @@ router.get('/users', function(req, res, next) {
       })
 });
 
-/* Hämtar specifik användare, ej klar, får bara Null
-router.post('/users', function(req, res, next) {
-  id = req.params.id;
-  console.log(id);
-  req.app.locals.db.collection("users").findOne({"id": new ObjectId(id)})
-  .then(result => {
-    console.log(result);
+/*Hämtar specifik användare*/
 
-     res.json(result)
-  })
+router.post('/users', async function(req, res, next) {
+  try {
+    let id = req.body.id;
+    console.log("id", id);
+    let result = await req.app.locals.db.collection("users").findOne({"_id": new ObjectId(id)});
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+  }
 });
-*/
 
 /*Lägger till användare*/
-router.post("/users/add", function(req, res) {
+router.post("/users/add", function(req, res) { 
   req.app.locals.db.collection("users").insertOne(req.body)
   .then(result => {
     console.log(result)
@@ -32,6 +33,29 @@ router.post("/users/add", function(req, res) {
 });
 
 /* Logga in användare */
+router.post("/users/login", function(req, res) {
+  const { email, password } = req.body;
+
+  req.app.locals.db.collection("users").findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.status(400).send({ message: 'Ogiltig epost' });
+        return;
+      }
+
+      if (user.password !== password) {
+        res.status(400).send({ message: 'Ogiltigt lösenord' });
+        return;
+      }
+
+      // Password is correct
+      res.status(200).send({ message: 'Rätt!' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send({ message: 'Servern gick åt helvete på nåt sätt' });
+    });
+});
 
 /* Hämtar ALLA produkter */
 router.get('/products', function(req, res, next) {
@@ -43,6 +67,23 @@ router.get('/products', function(req, res, next) {
 
 
 /* Hämta specifik produkt*/
+router.get("/api/products/:id", function(req, res) {
+  const productId = req.params.id;
+
+  req.app.locals.db.collection("products").findOne({ _id: ObjectId(productId) })
+    .then(product => {
+      if (!product) {
+        res.status(404).send({ message: 'Product not found' });
+        return;
+      }
+
+      res.status(200).send(product);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send({ message: 'Internal server error' });
+    });
+});
 
 
 /*Lägga till produkt*/
