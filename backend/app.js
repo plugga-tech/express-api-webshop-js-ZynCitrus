7724eee0ed76788fd07a2fcf4bca1876ff480b8d
Sebastian Post <sebastianpost@protonmail.com>
2023-04-21 13:37:02 +0200

    Localhost

--- backend/app.js
+++ backend/app.js
@@ -2,34 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var apiRouter = require('././routes/api');
-
-const url = 'mongodb://localhost:8889/userbook';
-
 var app = express();
-
 const MongoClient = require("mongodb").MongoClient;
 
+const url = "mongodb://localhost:27017/users";
 MongoClient.connect(url)
-  .then(client => {
-    console.log("Connected to database");
+.then(client => {
+    console.log("Ansluten till DB");
 
     const db = client.db("users");
     app.locals.db = db;
-  })
-  .catch(err => {
-    console.log(err);
-  });
+})
  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
 module.exports = app;
