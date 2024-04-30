84861ae79d4b5a5111c282d6debfeace496cb06d
Sebastian Post <115038252+ZynCitrus@users.noreply.github.com>
2024-04-30 14:22:13 +0200

    Missade lagersaldo

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const { ObjectId } = require("mongodb"); // För att använda ObjectId
 
-// Skapa en ny order
-// Route to handle order creation
-router.post("/add", function (req, res) {
-  req.app.locals.db
-    .collection("orders")
-    .insertOne(req.body)
-    .then((result) => {
-      console.log(result);
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send({ message: "Internal server error" });
-    });
+const { ObjectId } = require("mongodb");
+
+// Skapa en order och minska lagret för varje produkt
+router.post("/add", async (req, res) => {
+  try {
+    const { user, products } = req.body;
+
+    const db = req.app.locals.db;
+
+    for (const item of products) {
+      const { productId, quantity } = item;
+
+      await db
+        .collection("products")
+        .updateOne(
+          { _id: new ObjectId(productId) },
+          { $inc: { lager: -quantity } }
+        );
+    }
+    await db.collection("orders").insertOne({ user, products });
+
+    res
+      .status(201)
+      .json({ message: "Order skapad och lager minskad för produkterna." });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
-
 // Visa alla orders
 router.get("/all", async (req, res) => {
   try {
